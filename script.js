74be9c252e384d528a1bfe5aa2052ee478f17b50
Abdulraman Saumu <abdulrahmansaumu@gmail.com>
2025-06-14 09:38:19 +0300

    debugging 1 Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function (){
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
@@ -43,4 +43,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if (taskText === "") {
             alert("Please enter a task.");
             return;
-
+        }
+    });
+});
